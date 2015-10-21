9c2491b673b9059f3b50b5b444fb6d2ce86f0a35
Jens W. Klein <jk@kleinundpartner.at>
2015-10-22 01:26:18 +0200

    clean

--- src/pas/plugins/authomatic/utils.py
+++ src/pas/plugins/authomatic/utils.py
@@ -32,7 +32,6 @@ def authomatic_cfg():
     returns it as a dict
     """
     settings = authomatic_settings()
-    import pdb;pdb.set_trace();
     try:
         cfg = json.loads(settings.json_config)
     except ValueError:
