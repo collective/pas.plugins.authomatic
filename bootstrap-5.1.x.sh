e48790c9657b69c1f7cc8bcdf689afbfdf385e3e
Jens W. Klein <jk@kleinundpartner.at>
2017-03-31 17:23:35 +0200

    requirements

--- bootstrap-5.1.x.sh
+++ bootstrap-5.1.x.sh
@@ -2,5 +2,5 @@
 ln -fs plone-5.1.x.cfg buildout.cfg
 rm -r ./lib ./include ./local ./bin
 virtualenv --clear .
-./bin/pip install -r https://raw.githubusercontent.com/plone/buildout.coredev/5.1/requirements.txt
+./bin/pip install -r requirements.txt
 ./bin/buildout
